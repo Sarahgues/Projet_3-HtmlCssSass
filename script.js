2b9df0dbe6e083ba108bf5ddced73bc4b49c088a
user.name "barkhoum GUESMIA <smguesmia@gmail.com>
2021-04-20 19:54:02 +0200

    ajout code html et sass du bouton GET STARGET

--- script.js
+++ script.js
@@ -4,8 +4,5 @@ $("body").on("mousemove", function(t) {
     let vertical = -($(window).innerWidth() / 2 - t.pageX) / 70,
         horizontal = ($(window).innerHeight() / 2 - t.pageY) / 70;
 
-    card.attr(
-        "style",
-        "transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)"
-    );
+    card.attr("style","transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)")
 });
